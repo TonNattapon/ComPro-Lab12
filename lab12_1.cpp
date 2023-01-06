4024f94da2682dc93f189240d26edbfdb9f6eefb
= <=>
2023-01-06 15:29:19 +0700

    Complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    int n;
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"},input;
+    cout << "Press Enter 3 times to reveal your future.";
+    srand(time(0));
+    n = rand() % 9;
+    getline(cin,input);
+    getline(cin,input);
+    getline(cin,input);
+    cout << "You will get " << grade[n] << " in this 261102.";
+}
